86bd1a1b7d6165e20ffccc7f648807d3b8859733
Evandro Santos <vandersant7@gmail.com>
2024-07-04 21:07:34 -0300

    docs: update REDME

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ export const app = async (
   response: http.ServerResponse
 ) => {
   //queryString
-  const [baseUrl, queryString] = request.url?.split('?') ?? ['', '']
+  const baseUrl = request.url?.split('?')[0]
 
   // list podcasts
   if (request.method === HttpMethod.GET && baseUrl === Routes.LIST) {
